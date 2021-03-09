ec4bbab575eeb95e69663ce5d3471cd00edf01aa
Volkan Isik <vis@pantherapplications.com>
2021-03-09 08:28:53 +0100

    ny verion

---
+++ indexyaml.php
@@ -0,0 +1,123 @@
+<?php
+require_once "vendor/autoload.php";
+use Symfony\Component\Yaml\Yaml;
+
+$string = array("url"=>"","format"=>"csv");
+$string['url']= "https://raw.githubusercontent.com/VolkanIsik86/drawcsv/master/test.csv";
+$base = base64_encode(zlib_encode(urlencode(json_encode($string)),ZLIB_ENCODING_RAW));
+$link = "https://app.diagrams.net/?desc=".$base;
+echo $link;
+echo "\n";
+
+$filen=fopen("test.csv","w") or die("unable to open");
+
+$csvheader= file_get_contents("csvheader.csv");
+$csvheader.="\n";
+$yamlfile = file_get_contents("process.yaml");
+$iso = utf8_encode($yamlfile);
+
+$yaml =  Yaml::parse($iso);
+print_r($yaml);
+
+
+//$step = $xml->processTask;
+//
+//
+//
+//
+//$symTabel = array();
+//
+//for($i=0 ; sizeof($step) > $i; $i++){
+//    $csvheader.= $step[$i]["stepnr"] .",".$step[$i]["name"] ;
+//
+//    if ($step[$i]["task"]=="process_jumpProcess"){
+//        jump($i);
+//    }
+//
+//    elseif ($step[$i]["task"]=="process_startProcess"){
+//        processStart($i);
+//    }else{
+//        regularStep($i);
+//    }
+//
+//    $csvheader.="\n";
+//}
+//echo $csvheader;
+//
+//fwrite($filen,$csvheader);
+//fclose($filen);
+//
+//shell_exec("git config --global credential.helper 'cache --timeout 28800'");
+//
+//shell_exec("git commit -a -m 'ny verion'");
+//
+//echo shell_exec("git push");
+//
+////echo($step[2]->config->branchrulea);
+//
+////print_r($xml);
+//
+//function processStart($i){
+//    global $csvheader;
+//    global $step;
+//    $color="#97d077";
+//    if($i!=(sizeof($step)-1)) {
+//        $csvheader .= ",$color,#000000," . "ellipse," . "," . "\"" . ($i + 2) . "\"" . "," . ",";
+//    } else {
+//        $csvheader .= ",$color,#000000," . "ellipse," . "," . "," . ",";
+//    }
+//
+//}
+//
+//function regularStep($i){
+//    global $csvheader;
+//    global $step;
+//    $color="#dae8fc";
+//    if($i!=(sizeof($step)-1))
+//        $csvheader.=",$color,#000000," . "rectangle," . "," . "\"". ($i+2) . "\"" . "," . ",";
+//    else
+//        $csvheader.=",$color,#000000," . "rectangle," . "," ."," . ",";
+//
+//}
+//
+//
+// function jump($i){
+//     global $csvheader;
+//     global $step;
+//     $color="#ffe599";
+//     if($i!=(sizeof($step)-1)) {
+//         if ($step[$i]->config->processstepnr == -1) {
+//             if($step[$i]->config->branchrulea==1 && $step[$i]->config->branchoperator=="==" && $step[$i]->config->branchruleb==1) {
+//                 $csvheader .= ",$color,#000000," . "rhombus," . $step[$i]->config->branchrulea
+//                     . $step[$i]->config->branchoperator
+//                     . $step[$i]->config->branchruleb
+//                     . "," . "," . ",";
+//             }else{
+//                 $csvheader .= ",$color,#000000," . "rhombus," . $step[$i]->config->branchrulea
+//                     . $step[$i]->config->branchoperator
+//                     . $step[$i]->config->branchruleb
+//                     . "," . "," . ","
+//                     ."\"" . ($i + 2) . "\"";
+//             }
+//         }elseif ($step[$i]->config->branchrulea==1 && $step[$i]->config->branchoperator=="==" && $step[$i]->config->branchruleb==1){
+//             $csvheader .= ",$color,#000000," . "rhombus," . $step[$i]->config->branchrulea
+//                 . $step[$i]->config->branchoperator
+//                 . $step[$i]->config->branchruleb
+//                 . "," . "," . "\""
+//                 . $step[$i]->config->processstepnr
+//                 . "\"" . ","
+//                 ;
+//         }
+//         else {
+//             $csvheader .= ",$color,#000000," . "rhombus," . $step[$i]->config->branchrulea
+//                 . $step[$i]->config->branchoperator
+//                 . $step[$i]->config->branchruleb
+//                 . "," . "," . "\""
+//                 . $step[$i]->config->processstepnr
+//                 . "\"" . ","
+//                 . "\"" . ($i + 2) . "\"";
+//         }
+//     }else {
+//         $csvheader .= ",$color,#000000," . "rhombus," . "," . "," . ",";
+//     }
+// }
---
+++ vendor/composer/installed.php
@@ -0,0 +1,51 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '1600a22955336115398e17aba37ee390e615e7d0',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1600a22955336115398e17aba37ee390e615e7d0',
+    ),
+    'symfony/deprecation-contracts' => 
+    array (
+      'pretty_version' => 'v2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '5fa56b4074d1ae755beb55617ddafe6f5d78f665',
+    ),
+    'symfony/polyfill-ctype' => 
+    array (
+      'pretty_version' => 'v1.22.1',
+      'version' => '1.22.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'c6c942b1ac76c82448322025e084cadc56048b4e',
+    ),
+    'symfony/yaml' => 
+    array (
+      'pretty_version' => 'v5.2.4',
+      'version' => '5.2.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7d6ae0cce3c33965af681a4355f1c4de326ed277',
+    ),
+  ),
+);
